a26dabd4948e22019ec9c8eb05797710db00e0af
wyss.shi <wyss.shi@anker-in.com>
2023-06-13 11:56:38 +0800

    修改list_show,输出更多说明

--- list_show.py
+++ list_show.py
@@ -1,10 +1,17 @@
 #!usr/bin/env python3
 # -*- coding: utf-8  -*-
+from collections import OrderedDict
 
-dict = {'1. 多匹配多搜索脚本: sousou_8_8.py':'请使用python sousou_8_8.py -h 显示参数细节', 
-        '2. 统计出流的脚本:streamfind.py':'可以在弹窗选择多个脚本文件'}
+dict = OrderedDict()
+dict['1. sousou_8_8.py:多匹配多搜索脚本,请使用python sousou_8_8.py -h 显示参数细节'] = None
+dict['2. streamfind-HB2.py:统计出流的脚本,可以在弹窗选择多个脚本文件'] = None
+dict['3. record-HB2.py:统计HB2录像的脚本,可以在弹窗选择多个脚本文件'] = None
+dict['4. record-HB3.py:统计HB3录像的脚本,可以在弹窗选择多个脚本文件'] = None
+dict['5. statistics_hb2.py:统计HB2录像出流json的脚本,可以在弹窗选择多个脚本文件'] = None
 
 for dat in dict:
-    print(dat,':',dict[dat] )
+    print(dat)
+    print()
 
-
+if __name__ == '__main__':
+    input('Press any key to exit...')
